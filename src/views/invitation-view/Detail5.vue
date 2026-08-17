e8994059e1776ce15209f9cce578d4541edec93d
jevyies <jevyababa@gmail.com>
2026-08-17 13:57:29 +0800

    commit

--- src/views/invitation-view/Detail5.vue
+++ src/views/invitation-view/Detail5.vue
@@ -14,8 +14,9 @@ import img8 from '@/assets/prenup/8.jpg'
 import img9 from '@/assets/prenup/9.jpg'
 import img10 from '@/assets/prenup/10.jpg'
 import img11 from '@/assets/prenup/11.jpg'
+import img12 from '@/assets/prenup/12.jpg'
 
-const slides = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11]
+const slides = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12]
 const props = defineProps({
   guestId: {
     type: String,
